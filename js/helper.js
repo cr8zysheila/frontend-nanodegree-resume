5a602c59b5c8ad498b15a3bc3e96a78f0d59a1a6
MyName <me@example.com>
2016-11-20 22:36:29 -0800

    Final project plus two challenges

--- js/helper.js
+++ js/helper.js
@@ -62,6 +62,14 @@ var googleMap = '<div id="map"></div>';
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
+function inName(nameString)
+{
+  var names = nameString.split(" ");
+  names[0] = names[0].slice(0, 1).toUpperCase() + names[0].slice(1).toLowerCase();
+  names[1] = names[1].toUpperCase();
+  return names.join(" ");
+}
+
 $(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
@@ -86,6 +94,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
+  logClicks(loc.pageX, loc.pageY);
   // your code goes here!
 });
 
@@ -178,6 +187,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +249,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -47,7 +47,7 @@ var bio = {
 	"contacts": {
 		mobile: "435-666-8888",
 		email: "peppa@gmail.com",
-		github: "peppa.github.com"
+		github: "peppa.github.com",
 		location: "Redmond"
 	}
 };
@@ -85,7 +85,7 @@ var projects = {
 	},
 	{
 		title: "Project1",
-		dates: 2010
+		dates: 2010,
 		description: "First project",
 		images: [
 			"http://placehold.it/300x310",
@@ -120,3 +120,184 @@ var education = {
 };
 
 
+$("#header").prepend(HTMLskills.replace("%data%", bio["skills"]));
+$("#header").prepend(HTMLskillsStart);
+$("#header").prepend(HTMLwelcomeMsg.replace("%data%", bio["welcomeMessage"]));
+$("#header").prepend(HTMLbioPic.replace("%data%", bio["biopic"]));
+$("#header").prepend(HTMLemail.replace("%data%", bio.contacts["email"]));
+$("#header").prepend(HTMLheaderRole.replace("%data%", bio["role"]));
+$("#header").prepend(HTMLheaderName.replace("%data%", bio["name"])); //using bio[name] will get undefined 
+/*if(bio.skills)
+{
+	$("#header").append(HTMLskillsStart);
+	$("#skills").append(HTMLskills.replace("%data%", bio["skills"]));
+}*/
+
+/* It's bad practice using for-in on arrays
+for(job in work.jobs)
+{
+	$("#workExperience").append(HTMLworkStart);
+	var workEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var workTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+	var workDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	var workLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	var workDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+	$(".work-entry:last").append(workEmployer + workTitle);
+	$(".work-entry:last").append(workDate);
+	$(".work-entry:last").append(workLocation);
+	$(".work-entry:last").append(workDescription);
+
+}
+*/
+function displayWork()
+{
+	work.jobs.forEach( function (job) {
+
+		$("#workExperience").append(HTMLworkStart);
+		var workEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+		var workTitle = HTMLworkTitle.replace("%data%", job.title);
+
+		var workDate = HTMLworkDates.replace("%data%", job.dates);
+		var workLocation = HTMLworkLocation.replace("%data%", job.location);
+		var workDescription = HTMLworkDescription.replace("%data%", job.description);
+
+		$(".work-entry:last").append(workEmployer + workTitle);
+		$(".work-entry:last").append(workDate);
+		$(".work-entry:last").append(workLocation);
+		$(".work-entry:last").append(workDescription);
+
+	});
+}
+displayWork();
+
+projects.display = function(){
+	projects.projects.forEach(function( project ){
+
+
+		$("#projects").append(HTMLprojectStart);
+
+		var projectTitle = HTMLprojectTitle.replace("%data%", project.title);
+		var projectDates = HTMLprojectDates.replace("%data%", project.dates);
+		var projectDescription = HTMLprojectDescription.replace("%data%", project.description);
+		$(".project-entry:last").append(projectTitle);
+		$(".project-entry:last").append(projectDates);
+		$(".project-entry:last").append(projectDescription);
+
+
+		project.images.forEach(function(img){
+			projectImage = HTMLprojectImage.replace("%data%", img);
+			$(".project-entry:last").append(projectImage);
+		})
+	})
+}
+projects.display();
+
+education.display = function () {
+	education.schools.forEach(function( school ){
+		$("#education").append(HTMLschoolStart);
+		var schoolName = HTMLschoolName.replace("%data%", school.name);
+		schoolName = schoolName.replace("#", school.url);
+		var schoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+		var schoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+		var schoolDates = HTMLschoolDates.replace("%data%", school.date);
+		var shcoolMajor = HTMLschoolMajor.replace("%data%", school.majors);
+		$(".education-entry:last").append(schoolName + schoolDegree);
+		$(".education-entry:last").append(schoolLocation);
+		$(".education-entry:last").append(schoolDates);
+		$(".education-entry:last").append(shcoolMajor);
+	})
+}
+education.display();
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
+
+
+/* challenge 1
+function getRelationship(first, second)
+{
+	var isFirstNum = ( typeof first === 'number' && !isNaN(first) );
+	var isSecondNum = ( typeof second === 'number' && !isNaN(second));
+
+	if( !isFirstNum && isSecondNum)
+	{
+		return ("can't compare relationships because " + first +
+			" is not a number.");
+	}
+	else if( isFirstNum && !isSecondNum)
+	{
+		return ("can't compare relationships because " + second +
+			" is not a number.");
+	}
+	else if( !isFirstNum && !isSecondNum)
+	{
+		return ("can't compare relationships because " + first + " and " + second +
+			" are not numbers.");
+	}
+	else
+	{
+		if( first > second)
+		{
+			return (first + " > " + second);
+		}
+		else if( first < second)
+		{
+			return (first + " < " + second);
+		}
+		else( first === second)
+		{
+			return (first + " = " +second);
+		}
+	}
+
+}
+
+// Try logging these functions to test your code!
+console.log(getRelationship(1,4));
+console.log(getRelationship(1,1));
+console.log(getRelationship("that",2));
+console.log(getRelationship("this"," something else"));
+console.log(getRelationship(3));
+console.log(getRelationship("hi"));
+console.log(getRelationship(NaN));
+console.log(getRelationship(NaN, undefined));
+*/
+
+/* challenge 1
+var moonWalkers = [
+  "Neil Armstrong",
+  "Buzz Aldrin",
+  "Pete Conrad",
+  "Alan Bean",
+  "Alan Shepard",
+  "Edgar Mitchell",
+  "David Scott",
+  "James Irwin",
+  "John Young",
+  "Charles Duke",
+  "Eugene Cernan",
+  "Harrison Schmitt"
+];
+
+function normalizeName(name)
+{
+	var nameArray = name.split(" ");
+	//swap  first name  and last name
+	[nameArray[0], nameArray[1]] = [nameArray[1], nameArray[0]];
+	return nameArray.join(', ');
+}
+
+function alphabetizer(names) {
+    // Your code goes here!
+    names.forEach( function(name, index){
+    	names[index] = normalizeName(name);
+    });
+
+    return names.sort();
+}
+
+// Try logging your results to test your code!
+console.log(alphabetizer(moonWalkers));
+*/
